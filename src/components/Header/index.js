c5f339b418c0dc3a78d95de490f483061ffab3cf
Chris Dietrich <chrisdietrich3666@gmail.com>
2022-02-02 17:47:21 -0500

    test

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const Header = () => {
+  return <div className="header">Hello</div>;
+};
+
+export default Header;
---
+++ src/components/Slider/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const Slider = () => {
+  return <div className="slider">Slider</div>;
+};
+
+export default Slider;
