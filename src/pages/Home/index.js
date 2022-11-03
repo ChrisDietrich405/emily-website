b25d50360be77084b54c9c97472489fa6b5babe1
Chris <chrisdietrich3666@gmail.com>
2022-11-03 18:04:49 -0500

    feat added content to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,71 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import Slideshow from "../../components/Slideshow";
 import Cards from "../../components/Cards";
 import PictureCards from "../../components/PictureCards";
 import styles from "./styles.module.css";
 
+const EmPicture = window.location.origin + "/assets/images/vegfood.jpg";
+
 export default function Home() {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <>
       <Slideshow />
       <section className={`${styles.container} inner-container`}>
+        <div className={styles.intro}>
+          <img src={EmPicture} alt="Em photo" className={styles.photo} />
+          <div>
+            <h1>Hi, I'm Em</h1>
+            <p>
+              I have an immense passion for helping guide others toward a more
+              healthful and vibrant life. This passion has led me to pursue a
+              career as a Registered Dietitian, to obtain a Master's Degree in
+              Nutrition, and to learn as much as I can about balanced nutrition
+              and healthy lifestyle practices.
+              <a
+                href="#some"
+                onClick={() => setReadMore((prevState) => !prevState)}
+              >
+                {readMore ? "show less" : "read more"}
+              </a>
+            </p>
+            {readMore && (
+              <p id="some">
+                These healthy lifestyle practices I abbreviate as SPARC since
+                they are sure to add spark to your life. They are as follows:
+                1). Stress management & self-care 2). Plant-based nutrition &
+                purpose 3). Adequate hydration & activity 4). Restful sleep 5).
+                Connection & community Early in my career as a Dietitian at
+                several nursing homes in Baltimore City, I witnessed what seemed
+                like immense suffering and reduced quality of life in men and
+                women, some as young as myself at the time (late 20s) due to
+                debilitating diseases like stroke, Type 2 Diabetes, kidney
+                failure, and others. I felt at a loss of how to help them which
+                led me to want to find a way to help prevent others from
+                suffering this same fate. Finding a way to help empower,
+                educate, and support individuals on their journeys to achieving
+                optimal health became my passion and something I feel so
+                grateful to be able to do! Plant-based nutrition and other
+                healthy lifestyle practices can help us to look and feel our
+                best! Research has shown the numerous benefits to eating
+                plant-based or moving in that direction and embracing other
+                elements of SPARC. A few of the health problems that can be
+                lessened or resolved by embracing the SPARC practices include:
+                High blood pressure High cholesterol High fasting blood sugars
+                and elevated HbA1c Chronic diseases like Type 2 Diabetes and
+                Heart Disease Joint pain and inflammation Low mood and/or energy
+                Difficulty losing weight and/or maintaining ideal weight You
+                will be happy to hear that eating plant-based means no more
+                calorie counting, portion control, or feeling deprived! Poor
+                digestion Migraines
+              </p>
+            )}
+          </div>
+        </div>
+
         <Cards />
         <PictureCards />
       </section>
