7eb21f0b99fa8a304e6b84876d13522b62cdd9c6
Chris <chrisdietrich3666@gmail.com>
2022-11-16 16:51:37 -0500

    debug list component

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,47 +1,27 @@
-
-
 import React from 'react'
 
-const List = ({data, ordered}) => {
-   return (
-{ordered }
-    
-      // {ordered ? (<ol>
-      //   {data.map((item) => {
-      //     return (
-      //       <li>{item}</li>
-      //     )
-      //   })}
-      //   </ol>) : (<ul>
-      //           {data.map((item) => {
-      //             return <li>{item}</li>;
-      //           })}
-      //         </ul>)}
-      
-    
-   )
+const List = ({ordered, data}) => {
+  return (
+    <>
+        {ordered ? (
+            <ol>
+                {data.map((item) => {
+                    return (
+                        <li>{item}</li>
+                    )
+                })}
+            </ol>
+        ) : (
+            <ul>
+                {data.map((item) => {
+                    return (
+                        <li>{item}</li>
+                    )
+                })}
+            </ul>
+        )}
+    </>
+  )
 }
 
-
 export default List
-
-// export const List = ({ data, ordered }) => {
-//   if (ordered) {
-//     return (
-//       <ol>
-//         {data.map((item) => {
-//           return <li>{item}</li>;
-//         })}
-//       </ol>
-//     );
-//   }
-//   return (
-//     <ul>
-//       {data.map((item) => {
-//         return <li>{item}</li>;
-//       })}
-//     </ul>
-//   );
-// };
-
-
