27009cbb186d11cc5636c1c619cfb097bcbb455a
Chris <chrisdietrich3666@gmail.com>
2022-11-30 16:50:53 -0500

    feat styled home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <div className={styles.intro}>
           <img src={EmPicture} alt="Em photo" className={styles.photo} />
           <div className={styles.intro_text}>
-            <h1 className={styles.intro_title}>Hi, I'm Em!</h1>
+            <h1 className="page_title">"Hi, I'm Em!"</h1>
             <p>
               I have an immense passion for helping guide others toward a more
               healthful and vibrant life. This passion has led me to pursue a
@@ -31,7 +31,7 @@ export default function Home() {
               These healthy lifestyle practices I abbreviate as SPARC since they
               are sure to add spark to your life. They are as follows:
             </p>
-            <a
+            {/* <a
               href="#some"
               onClick={(e) => {
                 e.preventDefault();
@@ -39,38 +39,40 @@ export default function Home() {
               }}
             >
               {readMore ? "Show less" : "Read more"}
-            </a>
+            </a> */}
 
-            {readMore && (
-              <>
-                <List data={homeData} ordered />
-
-                <p>
-                  Early in my career as a Dietitian at several nursing homes in
-                  Baltimore City, I witnessed what seemed like immense suffering
-                  and reduced quality of life in men and women, some as young as
-                  myself at the time (late 20s) due to debilitating diseases
-                  like stroke, Type 2 Diabetes, kidney failure, and others. I
-                  felt at a loss of how to help them which led me to want to
-                  find a way to help prevent others from suffering this same
-                  fate. Finding a way to help empower, educate, and support
-                  individuals on their journeys to achieving optimal health
-                  became my passion and something I feel so grateful to be able
-                  to do!
-                </p>
-                <p>
-                  Plant-based nutrition and other healthy lifestyle practices
-                  can help us to look and feel our best! Research has shown the
-                  numerous benefits to eating plant-based or moving in that
-                  direction and embracing other elements of SPARC. A few of the
-                  health problems that can be lessened or resolved by embracing
-                  the SPARC practices include:
-                </p>
-                <List data={homeData2} />
-              </>
-            )}
+            {/* {readMore && ( */}
+            <>
+              <List data={homeData} ordered />
+            </>
+            {/* )} */}
           </div>
         </div>
+        <section className={styles.biography_section}>
+          <p>
+            Early in my career as a Dietitian at several nursing homes in
+            Baltimore City, I witnessed what seemed like immense suffering and
+            reduced quality of life in men and women, some as young as myself at
+            the time (late 20s) due to debilitating diseases like stroke, Type 2
+            Diabetes, kidney failure, and others. I felt at a loss of how to
+            help them which led me to want to find a way to help prevent others
+            from suffering this same fate. Finding a way to help empower,
+            educate, and support individuals on their journeys to achieving
+            optimal health became my passion and something I feel so grateful to
+            be able to do!
+          </p>
+          <p>
+            Plant-based nutrition and other healthy lifestyle practices can help
+            us to look and feel our best! Research has shown the numerous
+            benefits to eating plant-based or moving in that direction and
+            embracing other elements of SPARC. A few of the health problems that
+            can be lessened or resolved by embracing the SPARC practices
+            include:
+          </p>
+          <div className={styles.health_problem_list}>
+            <List data={homeData2} />
+          </div>
+        </section>
         <Cards />
         <PictureCards />
       </section>
--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -6,6 +6,8 @@ import { organizationData } from "./organizationData";
 import { organizationData2 } from "./organizationData";
 import { organizationData3 } from "./organizationData";
 
+import styles from "./styles.module.css"
+
 const bannerPath = `${window.location.origin}/assets/images/vegfood.jpg`;
 
 const Organizations = () => {
@@ -13,41 +15,46 @@ const Organizations = () => {
     <>
       <Banner picturePath={bannerPath} />
       <section className="inner-container">
-        <p>
+        <h1 className="page_title">
           I am dedicated to helping you achieve your optimal health, let’s work
           together to see how we can help conquer your health-related
           challenges!
+        </h1>
+        <div className={styles.consultation_package}>
           <h2>Initial consultation plus 1 follow-up session:</h2>
           <List data={organizationData} />
           <p>
             **Sessions to be within a 3- month timeframe from time of initial
             consultation
           </p>
+        </div>
+        <div className={styles.consultation_package}>
           <h2>Initial consultation plus 3 follow-up sessions</h2>
           <List data={organizationData2} />
+        </div>
+        <div className={styles.consultation_package}>
           <h2>Initial consultation plus 6 follow-up sessions:</h2>
           <List data={organizationData3} />
           <p>
             **Sessions to be within a 6-month timeframe from time of initial
             consultation
           </p>
-          Plant-Powered Weight Loss and Wellness Programs Nature’s Wellness
-          Path’s Plant-Powered Programs are right for you if you are: ● Sick of
-          dieting, counting calories, &amp; cutting portion sizes to try to lose
-          weight? ● Fed up with feeling like a failure for falling off track
-          with your diet or health-related goals? ● Dreaming of the day when you
-          will have more energy and less joint pain to do the activities you
-          enjoy? ● Wanting to improve your blood pressure, cholesterol, and
-          blood sugar without medications? ● Looking to improve your skin, gut,
-          and brain health? ● Curious about trying out a plant-based diet but
-          not sure how to get started? ● Ready to take greater control of your
-          health? You deserve to experience abundant energy &amp; optimal
-          health! I am happy and humbled to help guide you on your journey to
-          achieve such! **Options for 4-, 8-, and 12-week programs Book a free
-          discovery call with me today so we can discuss your health-related
-          challenges, goals, and how we can work together to get you on the path
-          to optimal health!
-        </p>
+        </div>
+        Plant-Powered Weight Loss and Wellness Programs Nature’s Wellness Path’s
+        Plant-Powered Programs are right for you if you are: ● Sick of dieting,
+        counting calories, &amp; cutting portion sizes to try to lose weight? ●
+        Fed up with feeling like a failure for falling off track with your diet
+        or health-related goals? ● Dreaming of the day when you will have more
+        energy and less joint pain to do the activities you enjoy? ● Wanting to
+        improve your blood pressure, cholesterol, and blood sugar without
+        medications? ● Looking to improve your skin, gut, and brain health? ●
+        Curious about trying out a plant-based diet but not sure how to get
+        started? ● Ready to take greater control of your health? You deserve to
+        experience abundant energy &amp; optimal health! I am happy and humbled
+        to help guide you on your journey to achieve such! **Options for 4-, 8-,
+        and 12-week programs Book a free discovery call with me today so we can
+        discuss your health-related challenges, goals, and how we can work
+        together to get you on the path to optimal health!
         {/* <Text data={organizationData} /> */}
       </section>
     </>
